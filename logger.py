4f3726be43f5edeca0848a2887fe5e4fff60a63b
Puppet Managed <abdul.rehman@gpu-devnode-2.us-central1-b.c.gpu-infra-dev.internal>
2026-02-09 18:48:45 +0000

    first dev

--- logger.py
+++ logger.py
@@ -1,6 +1,6 @@
 import logging
  
-# 配置日志器
+#test
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
